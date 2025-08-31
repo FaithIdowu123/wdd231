04d302d143c3a1fba9ea2ed520d3d3640202d3b8
FaithIdowu123 <ofaith@byupathway.edu>
2025-08-31 21:27:32 +0100

    week1

---
+++ scripts/navigation.js
@@ -0,0 +1,12 @@
+let year = new Date().getFullYear();
+document.querySelector("#currentyear").textContent = year;
+document.querySelector("#lastModified").textContent = "Last modified: " + document.lastModified;
+
+const navigation = document.querySelector("#navigation");
+const menu = document.querySelector("#menu");
+
+menu.addEventListener("click", function(){
+    navigation.classList.toggle("show");
+    menu.classList.toggle("show");
+});
+
