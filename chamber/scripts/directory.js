e6f09bc67d801a681e61849bda981d619aee6391
FaithIdowu123 <ofaith@byupathway.edu>
2025-09-09 01:52:40 +0100

    week02

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,15 +1,40 @@
-fetch("data/members.json")
-  .then(response => {
-    if (!response.ok) {
-      throw new Error("Could not fetch members.json");
-    }
-    return response.json();
-    
-  })
-  .then(members => {
-    console.log(members);
-  })
-  
-  .catch(error => {
-    console.error("Error loading JSON:", error);
+async function loadmembers() {
+  const response = await fetch("data/members.json");
+  if (!response.ok) {
+    throw new Error("Could not fetch members.json");
+  }
+  const members = await response.json();
+  return members;
+}
+
+loadmembers().then(data => {
+  displaycards(data);
+});
+
+function displaycards(members){
+  document.querySelector("#cards").innerHTML = ``;
+  members.forEach(member => {
+     const card = document.createElement("section");
+     const title = document.createElement("p");
+     const image = document.createElement("img");
+     const address = document.createElement("p");
+     const number = document.createElement("p");
+     const link = document.createElement("a")
+
+     title.textContent = member.name;
+     image.setAttribute("src", member.image);
+     image.setAttribute("alt", member.name);
+     address.textContent = member.address;
+     number.textContent = member.phone;
+     link.setAttribute("href", member.website);
+     link.textContent = "Link to page";
+
+     card.appendChild(title);
+     card.appendChild(image);
+     card.appendChild(address);
+     card.appendChild(number);
+     card.appendChild(link);
+
+     document.querySelector("#cards").appendChild(card);
   });
+}
--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -3,9 +3,27 @@ document.querySelector("#currentyear").textContent = year;
 document.querySelector("#lastModified").textContent = "Last modified: " + document.lastModified;
 
 const navigation = document.querySelector("#navigation");
+const navanimation = document.querySelector("#animated")
 const menu = document.querySelector("#menu");
+const grid = document.querySelector("#grid");
+const list = document.querySelector("#list");
+const cards = document.querySelector("#cards")
+const main = document.querySelector("main");
 
 menu.addEventListener("click", function(){
+    navanimation.classList.toggle("show");
     navigation.classList.toggle("show");
     menu.classList.toggle("show");
 });
+
+grid.addEventListener("click", function() {
+    cards.classList.remove("list");
+    cards.classList.add("grid");
+    main.classList.remove("list");
+});
+
+list.addEventListener("click", function() {
+    cards.classList.remove("grid");
+    cards.classList.add("list");
+    main.classList.add("list")
+})
