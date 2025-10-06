800493c199224befa985cc77fb807e90c6962271
FaithIdowu123 <ofaith@byupathway.edu>
2025-10-06 01:40:23 +0100

    fix

--- project/scripts/spotlight.js
+++ project/scripts/spotlight.js
@@ -2,16 +2,16 @@ import { displayFeatured, displaymodel, apiFetch, getRandomInt } from "./gameque
 
 const url = `https://corsproxy.io/?https://www.freetogame.com/api/games?sort-by=popularity`;
 const timestampField = document.getElementById("formLoadTime");
-let comments =  JSON.parse(localStorage.getItem("comments")) || [];
+let comments = JSON.parse(localStorage.getItem("comments")) || [];
 
 apiFetch(url).then(data => {
-    let random = getRandomInt(0, data.length - 1);
-    displayFeatured(data,random, random + 2);
-})
+  const random = getRandomInt(0, data.length - 1);
+  displayFeatured(data, random, random + 2);
+});
 
-document.addEventListener("DOMContentLoaded", function() {
-    var currentTimestamp = new Date().toISOString();
-    timestampField.value = currentTimestamp;
+document.addEventListener("DOMContentLoaded", () => {
+  const currentTimestamp = new Date().toISOString();
+  timestampField.value = currentTimestamp;
 });
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -23,12 +23,20 @@ document.addEventListener("DOMContentLoaded", () => {
   const text = document.getElementById("text");
 
   form.addEventListener("submit", (e) => {
-    let comment = [fname.value + " " + lname.value, email.value, topic.value, text.value, timestampField.value];
-    comments.push();
+    e.preventDefault();
+    const comment = [
+      `${fname.value} ${lname.value}`,
+      email.value,
+      topic.value,
+      text.value,
+      timestampField.value
+    ];
+    comments.push(comment);
     setvisits();
+    form.reset();
   });
 });
 
-function setvisits(){
-    localStorage.setItem("comments", JSON.stringify(comments));
+function setvisits() {
+  localStorage.setItem("comments", JSON.stringify(comments));
 }
